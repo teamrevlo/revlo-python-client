56cbfa7454e401b4460b7bf13758e77cab2a6040
Scott Nguyen <scott@revlo.co>
2016-11-29 18:42:25 -0800

    setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'revlo',
   packages = ['revlo'],
-  version = '0.14',
+  version = '0.14.1',
   description = 'Revlo Python Client',
   author = 'Scott Nguyen',
   author_email = 'scott.q.nguyen@gmail.com',
   url = 'https://github.com/teamrevlo/revlo-python-client',
-  download_url = 'https://github.com/teamrevlo/revlo-python-client/tarball/0.14',
+  download_url = 'https://github.com/teamrevlo/revlo-python-client/tarball/0.14.1',
   keywords = ['revlo-api', 'revlo', 'revloapi'],
   classifiers = [
     'License :: OSI Approved :: MIT License',
